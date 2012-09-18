de03aa0790de7e785ad7b8d9bb62803e39ee1a40
Alex Clark <aclark@aclark.net>
2012-09-18 13:20:58 -0400

    Release 1.2.0

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
+import os
 
-version = '1.1'
+version = '1.2.0'
 
 setup(name='collective.portlet.tal',
       version=version,
       description="TAL portlet for Plone 3 and 4",
       long_description="""\
 This portlet allows you to enter TAL into a text area, which is then executed
-as if it came from a page template""",
+as if it came from a page template\n""" + "\n" + open(os.path.join("docs", "HISTORY.txt")).read() ,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
@@ -19,6 +20,8 @@ as if it came from a page template""",
       keywords='plone portlet tal',
       author='Martin Aspeli',
       author_email='optilude@gmx.net',
+      maintainer='Alex Clark',
+      maintainer_email='aclark@aclark.net',
       url='http://plone.org',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
@@ -31,6 +34,7 @@ as if it came from a page template""",
           'Products.PloneTestCase',
       ],
       entry_points="""
-      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
